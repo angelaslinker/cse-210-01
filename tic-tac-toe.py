516ce2199fc0fe828eb41b509903b7fada086604
angelaslinker <angela.r.slinker@gmail.com>
2022-05-04 14:14:23 -0700

    added main function

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -12,6 +12,17 @@ currentPlayer = "X"
 winner = None
 gameRunning = True
 
+def main():
+    while gameRunning:
+        print_game_board(board)
+        playerinput(board)
+        check_winner(board)
+        check_tie(board)
+        switchPlayer()
+        computer_player(board)
+        check_winner(board)
+        check_tie(board)
+
 
 def print_game_board(board):
     print(board[0], "|", board[1], "|", board[2])
