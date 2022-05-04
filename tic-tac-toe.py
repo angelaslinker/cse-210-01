3f779c7c38eeb458c4d3fbd90f2ff414a3d77644
angelaslinker <angela.r.slinker@gmail.com>
2022-05-04 13:51:16 -0700

    created board and some variables

---
+++ tic-tac-toe.py
@@ -0,0 +1,13 @@
+#CSE 210 Tic Tac Toe
+#Angela Slinker
+
+import random
+from pickle import TRUE
+
+board = ["-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-"]
+
+currentPlayer = "X"
+winner = None
+gameRunning = True
