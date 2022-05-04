d255b2beb49c6ee5abc7b660e6551195268bb0af
angelaslinker <angela.r.slinker@gmail.com>
2022-05-04 14:03:26 -0700

    pring game board function

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -11,3 +11,11 @@ board = ["-", "-", "-",
 currentPlayer = "X"
 winner = None
 gameRunning = True
+
+
+def print_game_board(board):
+    print(board[0], "|", board[1], "|", board[2])
+    print("---------")
+    print(board[3], "|", board[4], "|", board[5])
+    print("---------")
+    print(board[6], "|", board[7], "|", board[8])
